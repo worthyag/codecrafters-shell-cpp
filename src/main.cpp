e3f5ab56bdf2b69511d87743acba8cf5633ff8ca
Worthy <worthyintech@gmail.com>
2024-07-03 14:58:31 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+#include <array>
+
+bool isValidCommand(std::string input);
+std::string getResponse(std::string command);
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,4 +15,27 @@ int main() {
   
   std::string input;
   std::getline(std::cin, input);
+
+  // Simple solution.
+  // std::cout << input << ": command not found";
+  // std::cout << std::unitbuf;
+
+  std::cout << getResponse(input) << std::endl;
+  std::cout << std::unitbuf;
+}
+
+bool isValidCommand(std::string input) {
+  std::string commands[] {"echo", "cd"};
+  std::string* end {commands + (sizeof(commands) / sizeof(commands[0]))};
+
+  for (std::string* ptr = commands; ptr != end; ++ptr) {
+    if (*ptr == input) return true;
+  }
+
+  return false;
+}
+
+std::string getResponse(std::string command) {
+  if (!isValidCommand(command)) return (command + ": command not found");
+  else return "processing";
 }
