f5f796d429f964b708fd5c849513211de24372cc
Worthy <worthyintech@gmail.com>
2024-07-03 15:21:27 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void echo(std::string input);
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -13,8 +15,15 @@ int main() {
     std::getline(std::cin, input);
 
     if (input == "exit 0") return 0;
-
-    std::cout << input << ": command not found\n";
-    std::cout << std::unitbuf;
+    else if (input.substr(0, 4) == "echo") echo(input.substr(5, input.length() - 5));
+    else {
+      std::cout << input << ": command not found\n";
+      std::cout << std::unitbuf;
+    }
   }
+}
+
+void echo(std::string input) {
+  std::cout << input << std::endl;
+  std::cout << std::unitbuf;
 }
